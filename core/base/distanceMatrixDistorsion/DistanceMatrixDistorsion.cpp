a8f8ab1a44b03186763aab2ec1ba680e4a482387
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2023-01-11 15:49:30 +0100

    sim module: empty templates

---
+++ core/base/distanceMatrixDistorsion/DistanceMatrixDistorsion.cpp
@@ -0,0 +1,6 @@
+#include <DistanceMatrixDistorsion.h>
+
+ttk::DistanceMatrixDistorsion::DistanceMatrixDistorsion() {
+  // inherited from Debug: prefix will be printed at the beginning of every msg
+  this->setDebugMsgPrefix("DistanceMatrixDistorsion");
+}

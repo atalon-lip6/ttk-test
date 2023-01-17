0b0cba689c13947e7c0129322db3c8b371ff5b83
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2023-01-17 14:13:29 +0100

    gestion diff proche de zéro et choses mineures

--- core/base/distanceMatrixDistorsion/DistanceMatrixDistorsion.cpp
+++ core/base/distanceMatrixDistorsion/DistanceMatrixDistorsion.cpp
@@ -34,9 +34,26 @@ int ttk::DistanceMatrixDistorsion::execute(const std::vector<std::vector<double>
   // Actually we first compute delta(x,y) = (d_l(x,y)-d_h(x,y))^2
   double maxi = 0;
   std::vector<std::vector<double>> deltaBis(lowDistMatrix.size());
+
+  this->printMsg("\n La high matrice :\n\n");
   for (int i = 0; i < n; i++)
   {
+    for (int j = 0; j < n; j++)
+      this->printMsg("\t"+std::to_string(highDistMatrix[i][j]));
+    this->printMsg("\n");
+  }
+  this->printMsg("\n Et la low matrice :\n\n");
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+      this->printMsg("\t"+std::to_string(lowDistMatrix[i][j]));
+    this->printMsg("\n");
+  }
+
+
 #ifndef TTK_ENABLE_KAMIKAZE
+  for (int i = 0; i < n; i++)
+  {
     if (highDistMatrix[i].size() != n)
     {
       this->printMsg(" Sizes mismatch: high distance matrix is not a square matrix: it has " + std::to_string(n) + " rows and  row " + std::to_string(i) + " has " + std::to_string(highDistMatrix[i].size()) + " elements.\n");
@@ -47,8 +64,12 @@ int ttk::DistanceMatrixDistorsion::execute(const std::vector<std::vector<double>
       this->printMsg(" Sizes mismatch: low distance matrix is not a square matrix: it has " + std::to_string(n) + " rows and  row " + std::to_string(i) + " has " + std::to_string(lowDistMatrix[i].size()) + "elements .\n");
       return 0;
     }
+  }
 #endif
 
+
+  for (int i = 0; i < n; i++)
+  {
     deltaBis[i].resize(n);
 
     for (int j = 0; j < n; j++)
@@ -58,6 +79,9 @@ int ttk::DistanceMatrixDistorsion::execute(const std::vector<std::vector<double>
       maxi = std::max(maxi, deltaBis[i][j]);
     }
   }
+  if (maxi < 1e-8)
+    maxi = 1;
+  //TODO et si maxi ~= 0 ?
 
 
   // Then the actual delta'(x,y) = 1-(delta(x,y)/max{delta}).
@@ -74,11 +98,13 @@ int ttk::DistanceMatrixDistorsion::execute(const std::vector<std::vector<double>
     totalSum += sum;
   }
 
-  this->printMsg("Computed distorsion value :", totalSum/(n*n));
+  this->printMsg("Size of output in ttk/base = " + std::to_string(distorsionVerticesValues.size()) + "\n");
+
+  this->printMsg("Computed distorsion value: " + std::to_string(totalSum/(n*n)));
   distorsionValue = totalSum/(n*n);
   this->printMsg(ttk::debug::Separator::L2); // horizontal '-' separator
   this->printMsg("Complete", 1, timer.getElapsedTime());
   this->printMsg(ttk::debug::Separator::L1); // horizontal '=' separator
 
-return 1;
+  return 1;
 }

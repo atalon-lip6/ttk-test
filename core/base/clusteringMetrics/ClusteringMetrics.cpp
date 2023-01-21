497e8b2724fa3d10e08041f8be8168a8af73dcc2
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2023-01-21 20:50:35 +0100

    code de base du nouveau module

---
+++ core/base/clusteringMetrics/ClusteringMetrics.cpp
@@ -0,0 +1,6 @@
+#include <ClusteringMetrics.h>
+
+ttk::ClusteringMetrics::ClusteringMetrics() {
+  // inherited from Debug: prefix will be printed at the beginning of every msg
+  this->setDebugMsgPrefix("ClusteringMetrics");
+}

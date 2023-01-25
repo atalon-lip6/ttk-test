51f8d63431de9d176cf2ba0ea35f1d54ca65b88a
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2023-01-25 11:40:08 +0100

    marche un peu mieux

--- core/base/clusteringMetrics/ClusteringMetrics.cpp
+++ core/base/clusteringMetrics/ClusteringMetrics.cpp
@@ -13,22 +13,30 @@ inline int nChoose2(int x)
   return x*(x-1)/2;
 }
 
-int ttk::ClusteringMetrics::compute_contingency_tables(const std::vector<double> &clust1, const std::vector<double> &clust2, std::vector<std::vector<double>> &contingencyMatrix, std::vector<double> &sumLin, std::vector<double> &sumCol) const
+int ttk::ClusteringMetrics::compute_contingency_tables(const std::vector<int> &clust1, const std::vector<int> &clust2, std::vector<std::vector<int>> &contingencyMatrix, std::vector<int> &sumLin, std::vector<int> &sumCol) const
 {
   size_t n = clust1.size();
 
-  int max1 = *std::max_element(clust1.begin(), clust1.end());
-  int max2 = *std::max_element(clust2.begin(), clust2.end());
-  int nCluster = std::max(max1, max2);
-  contingencyMatrix.resize(nCluster);
-  for (auto i = 0; i < nCluster; i++)
-    contingencyMatrix[i].resize(nCluster, 0);
+  this->printMsg("toto0: "+std::to_string(clust1.size())+"\n");
+  this->printMsg("toto0bis: "+std::to_string(clust2.size())+"\n");
 
-  for (auto i = 0; i < n; i++)
+  size_t max1 = *std::max_element(clust1.begin(), clust1.end());
+  size_t max2 = *std::max_element(clust2.begin(), clust2.end());
+  size_t nCluster = std::max(max1, max2)+1;
+  contingencyMatrix.resize(nCluster);
+  for (size_t i = 0; i < nCluster; i++)
+    contingencyMatrix[i].resize(nCluster, 0);
+  sumLin.resize(nCluster);
+  sumCol.resize(nCluster);
+
+  this->printMsg("toto1: "+std::to_string(contingencyMatrix.size())+"\n");
+
+  for (size_t i = 0; i < n; i++)
   {
     int x1 = clust1[i], x2 = clust2[i];
     contingencyMatrix[x1][x2]++;
   }
+  this->printMsg("toto1.5\n");
 
   for (size_t i1 = 0; i1 < nCluster; i1++)
   {
@@ -38,6 +46,7 @@ int ttk::ClusteringMetrics::compute_contingency_tables(const std::vector<double>
 
     sumLin[i1] = sum;
   }
+  this->printMsg("toto2\n");
 
   //TODO changer cache miss...
   for (size_t i2 = 0; i2 < nCluster; i2++)
@@ -48,11 +57,12 @@ int ttk::ClusteringMetrics::compute_contingency_tables(const std::vector<double>
 
     sumCol[i2] = sum;
   }
+  this->printMsg("toto2\n");
   return 0;
 }
 
 
-int ttk::ClusteringMetrics::compute_ARI(std::vector<std::vector<double>> &contingencyMatrix, const std::vector<double> &sumLin, const std::vector<double> &sumCol, double &ariValue) const
+int ttk::ClusteringMetrics::compute_ARI(std::vector<std::vector<int>> &contingencyMatrix, const std::vector<int> &sumLin, const std::vector<int> &sumCol, double &ariValue) const
 {
   size_t nCluster = contingencyMatrix.size();
 
@@ -81,7 +91,7 @@ int ttk::ClusteringMetrics::compute_ARI(std::vector<std::vector<double>> &contin
 }
 
 
-int ttk::ClusteringMetrics::compute_NMI(std::vector<std::vector<double>> &contingencyMatrix, const std::vector<double> &sumLin, const std::vector<double> &sumCol, double &nmiValue) const
+int ttk::ClusteringMetrics::compute_NMI(std::vector<std::vector<int>> &contingencyMatrix, const std::vector<int> &sumLin, const std::vector<int> &sumCol, double &nmiValue) const
 {
   size_t nCluster = contingencyMatrix.size();
 
@@ -91,6 +101,8 @@ int ttk::ClusteringMetrics::compute_NMI(std::vector<std::vector<double>> &contin
   {
     for (size_t i2 = 0; i2 < nCluster; i2++)
     {
+      if (contingencyMatrix[i1][i2] == 0)
+        continue;
       double logArg = contingencyMatrix[i1][i2]/(sumLin[i1]*sumCol[i2]);
       double curAdd = contingencyMatrix[i1][i2]*log2(logArg);
       mutualInfo += curAdd;
@@ -100,11 +112,71 @@ int ttk::ClusteringMetrics::compute_NMI(std::vector<std::vector<double>> &contin
   double entropy1 = 0, entropy2 = 0;
   for (size_t i = 0; i < nCluster; i++)
   {
-    entropy1 -= sumLin[i]*log2(sumLin[i]);
-    entropy2 -= sumCol[i]*log2(sumCol[i]);
+    //TODO plutôt assert plus tôt, je devrait pas arriver
+    if (sumLin[i] != 0)
+      entropy1 -= sumLin[i]*log2(sumLin[i]);
+    if (sumCol[i] != 0)
+      entropy2 -= sumCol[i]*log2(sumCol[i]);
   }
 
   nmiValue = 2*mutualInfo/(entropy1+entropy2);
 
+  return 0;
+}
+
+
+  int ttk::ClusteringMetrics::execute(const std::vector<int> &clustering1, const std::vector<int> &clustering2, double &nmiValue, double &ariValue) const
+{
+  ttk::Timer timer;
+  size_t n = clustering1.size();
+
+#ifndef TTK_ENABLE_KAMIKAZE
+  // print horizontal separator
+  this->printMsg(ttk::debug::Separator::L1); // L1 is the '=' separator
+                                             // print input parameters in table format
+  this->printMsg({
+      {"#Threads", std::to_string(this->threadNumber_)},
+      {"#Vertices", std::to_string(n)},
+      });
+  this->printMsg(ttk::debug::Separator::L1);
+
+  if (clustering2.size() != n)
+  {
+    this->printMsg(" Sizes mismatch: clustering one represents " + std::to_string(n) + " points and clustering two represents " + std::to_string(clustering2.size()) + " points\n");
+    return 1;
+  }
+
+
+#endif
+
+  /*
+  this->printMsg("\n La high matrice :\n\n");
+  for (size_t i = 0; i < n; i++)
+  {
+    this->printMsg("\t"+std::to_string(clustering1[i]));
+  }
+  this->printMsg("\n Et la low matrice :\n\n");
+  for (size_t i = 0; i < n; i++)
+  {
+    this->printMsg("\t"+std::to_string(clustering2[i]));
+  }
+  */
+
+  std::vector<std::vector<int>> contingencyMatrix;
+  std::vector<int> sumLines, sumColumns;
+  compute_contingency_tables(clustering1, clustering2, contingencyMatrix, sumLines, sumColumns);
+
+  compute_ARI(contingencyMatrix, sumLines, sumColumns, ariValue);
+  compute_NMI(contingencyMatrix, sumLines, sumColumns, nmiValue);
+
+#ifndef TTK_ENABLE_KAMIKAZE
+  this->printMsg("Size of output in ttk/base = 2\n");
+
+  this->printMsg("Computed NMI value: " + std::to_string(nmiValue)+"\n");
+  this->printMsg("Computed ARI value: " + std::to_string(ariValue)+"\n");
+  this->printMsg(ttk::debug::Separator::L2); // horizontal '-' separator
+  this->printMsg("Complete", 1, timer.getElapsedTime());
+  this->printMsg(ttk::debug::Separator::L1); // horizontal '=' separator
+#endif
   return 0;
 }
--- core/base/clusteringMetrics/ClusteringMetrics.h
+++ core/base/clusteringMetrics/ClusteringMetrics.h
@@ -34,103 +34,13 @@ namespace ttk {
   public:
     ClusteringMetrics();
 
-    /**
-     * TODO 2: This method preconditions the triangulation for all operations
-     *         the algorithm of this module requires. For instance,
-     *         preconditionVertexNeighbors, preconditionBoundaryEdges, ...
-     *
-     *         Note: If the algorithm does not require a triangulation then
-     *               this method can be deleted.
-     */
-    int preconditionTriangulation(
-      ttk::AbstractTriangulation *triangulation) const {
-      return triangulation->preconditionVertexNeighbors();
-    }
+  int execute(const std::vector<int> &clustering1, const std::vector<int> &clustering2, double &nmiValue, double &ariValue) const;
 
-    /**
-     * TODO 3: Implementation of the algorithm.
-     *
-     *         Note: If the algorithm requires a triangulation then this
-     *               method must be called after the triangulation has been
-     *               preconditioned for the upcoming operations.
-     */
-    template <class dataType,
-              class triangulationType = ttk::AbstractTriangulation>
-    int computeAverages(dataType *outputData,
-                        const dataType *inputData,
-                        const triangulationType *triangulation) const {
-      // start global timer
-      ttk::Timer globalTimer;
-
-      // print horizontal separator
-      this->printMsg(ttk::debug::Separator::L1); // L1 is the '=' separator
-
-      // print input parameters in table format
-      this->printMsg({
-        {"#Threads", std::to_string(this->threadNumber_)},
-        {"#Vertices", std::to_string(triangulation->getNumberOfVertices())},
-      });
-      this->printMsg(ttk::debug::Separator::L1);
-
-      // -----------------------------------------------------------------------
-      // Compute Vertex Averages
-      // -----------------------------------------------------------------------
-      {
-        // start a local timer for this subprocedure
-        ttk::Timer localTimer;
-
-        // print the progress of the current subprocedure (currently 0%)
-        this->printMsg("Computing Averages",
-                       0, // progress form 0-1
-                       0, // elapsed time so far
-                       this->threadNumber_, ttk::debug::LineMode::REPLACE);
-
-        // compute the average of each vertex in parallel
-        size_t nVertices = triangulation->getNumberOfVertices();
-#ifdef TTK_ENABLE_OPENMP
-#pragma omp parallel for num_threads(this->threadNumber_)
-#endif
-        for(size_t i = 0; i < nVertices; i++) {
-          // initialize average
-          outputData[i] = inputData[i];
-
-          // add neighbor values to average
-          size_t nNeighbors = triangulation->getVertexNeighborNumber(i);
-          ttk::SimplexId neighborId{-1};
-          for(size_t j = 0; j < nNeighbors; j++) {
-            triangulation->getVertexNeighbor(i, j, neighborId);
-            outputData[i] += inputData[neighborId];
-          }
-
-          // divide by neighbor number
-          outputData[i] /= (nNeighbors + 1);
-        }
-
-        // print the progress of the current subprocedure with elapsed time
-        this->printMsg("Computing Averages",
-                       1, // progress
-                       localTimer.getElapsedTime(), this->threadNumber_);
-      }
-
-      // ---------------------------------------------------------------------
-      // print global performance
-      // ---------------------------------------------------------------------
-      {
-        this->printMsg(ttk::debug::Separator::L2); // horizontal '-' separator
-        this->printMsg(
-          "Complete", 1, globalTimer.getElapsedTime() // global progress, time
-        );
-        this->printMsg(ttk::debug::Separator::L1); // horizontal '=' separator
-      }
-
-      return 1; // return success
-    }
-
-  int compute_contingency_tables(const std::vector<double> &clust1, const std::vector<double> &clust2, std::vector<std::vector<double>> &contingencyMatrix, std::vector<double> &sumLin, std::vector<double> &sumCol) const;
+  int compute_contingency_tables(const std::vector<int> &clust1, const std::vector<int> &clust2, std::vector<std::vector<int>> &contingencyMatrix, std::vector<int> &sumLin, std::vector<int> &sumCol) const;
 
 
-  int compute_ARI(std::vector<std::vector<double>> &contingencyMatrix, const std::vector<double> &sumLin, const std::vector<double> &sumCol, double &ariValue) const;
-  int compute_NMI(std::vector<std::vector<double>> &contingencyMatrix, const std::vector<double> &sumLin, const std::vector<double> &sumCol, double &nmiValue) const;
+  int compute_ARI(std::vector<std::vector<int>> &contingencyMatrix, const std::vector<int> &sumLin, const std::vector<int> &sumCol, double &ariValue) const;
+  int compute_NMI(std::vector<std::vector<int>> &contingencyMatrix, const std::vector<int> &sumLin, const std::vector<int> &sumCol, double &nmiValue) const;
   }; // ClusteringMetrics class
 
 } // namespace ttk

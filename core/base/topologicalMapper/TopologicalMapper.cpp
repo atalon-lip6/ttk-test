98cd0973bddd7e71a0c885679f4a201fc3ce19a7
Alexandre Talon <alexandre.talon@lip6.fr>
2023-03-14 18:18:02 +0100

    tout début, juste cloné de helloworld

---
+++ core/base/topologicalMapper/TopologicalMapper.cpp
@@ -0,0 +1,6 @@
+#include <TopologicalMapper.h>
+
+ttk::TopologicalMapper::TopologicalMapper() {
+  // inherited from Debug: prefix will be printed at the beginning of every msg
+  this->setDebugMsgPrefix("TopologicalMapper");
+}

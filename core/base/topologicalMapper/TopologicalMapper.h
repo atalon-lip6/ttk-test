6512ac315fe44ff024493c9a8ca98b7de00fb3cf
Alexandre Talon <alexandre.talon@lip6.fr>
2023-03-22 19:12:18 +0100

    premier jet, il manque des choses

--- core/base/topologicalMapper/TopologicalMapper.h
+++ core/base/topologicalMapper/TopologicalMapper.h
@@ -1,129 +1,39 @@
-/// TODO 1: Provide your information
-///
 /// \ingroup base
 /// \class ttk::TopologicalMapper
-/// \author Your Name Here <your.email@address.here>
-/// \date The Date Here.
+/// \author Alexandre Talon <alexandre.talon@lip6.fr>
+/// \date March 2023
 ///
-/// This module defines the %TopologicalMapper class that computes for each vertex of a
-/// triangulation the average scalar value of itself and its direct neighbors.
+///
+///TODO
+/// This module defines the %TopologicalMapper class that computes a
+/// score indicating how good the low dimension distance matrix represents the
+/// high dimension one. The score is computed according to the SIM formula.
+///
 ///
 /// \b Related \b publication: \n
-/// 'TopologicalMapper'
-/// Jonas Lukasczyk and Julien Tierny.
-/// TTK Publications.
-/// 2021.
+/// 'Principal Geodesic Analysis of Merge Trees (and Persistence Diagrams)'
+/// Mathieu Pont, Jules Vidal and Julien Tierny.
+/// IEEE Transactions on Visualization and Computer Graphics, 2023.
 ///
+/// \sa TopologicalMapper
 
 #pragma once
 
+#include <vector>
 // ttk common includes
 #include <Debug.h>
-#include <Triangulation.h>
+#include <DimensionReduction.h>
 
 namespace ttk {
 
   /**
-   * The TopologicalMapper class provides methods to compute for each vertex of a
-   * triangulation the average scalar value of itself and its direct neighbors.
+   * The TopologicalMapper class TODO
    */
-  class TopologicalMapper : virtual public Debug {
+  class TopologicalMapper : virtual public Debug, public DimensionReduction {
 
   public:
     TopologicalMapper();
 
-    /**
-     * TODO 2: This method preconditions the triangulation for all operations
-     *         the algorithm of this module requires. For instance,
-     *         preconditionVertexNeighbors, preconditionBoundaryEdges, ...
-     *
-     *         Note: If the algorithm does not require a triangulation then
-     *               this method can be deleted.
-     */
-    int preconditionTriangulation(
-      ttk::AbstractTriangulation *triangulation) const {
-      return triangulation->preconditionVertexNeighbors();
-    }
-
-    /**
-     * TODO 3: Implementation of the algorithm.
-     *
-     *         Note: If the algorithm requires a triangulation then this
-     *               method must be called after the triangulation has been
-     *               preconditioned for the upcoming operations.
-     */
-    template <class dataType,
-              class triangulationType = ttk::AbstractTriangulation>
-    int computeAverages(dataType *outputData,
-                        const dataType *inputData,
-                        const triangulationType *triangulation) const {
-      // start global timer
-      ttk::Timer globalTimer;
-
-      // print horizontal separator
-      this->printMsg(ttk::debug::Separator::L1); // L1 is the '=' separator
-
-      // print input parameters in table format
-      this->printMsg({
-        {"#Threads", std::to_string(this->threadNumber_)},
-        {"#Vertices", std::to_string(triangulation->getNumberOfVertices())},
-      });
-      this->printMsg(ttk::debug::Separator::L1);
-
-      // -----------------------------------------------------------------------
-      // Compute Vertex Averages
-      // -----------------------------------------------------------------------
-      {
-        // start a local timer for this subprocedure
-        ttk::Timer localTimer;
-
-        // print the progress of the current subprocedure (currently 0%)
-        this->printMsg("Computing Averages",
-                       0, // progress form 0-1
-                       0, // elapsed time so far
-                       this->threadNumber_, ttk::debug::LineMode::REPLACE);
-
-        // compute the average of each vertex in parallel
-        size_t nVertices = triangulation->getNumberOfVertices();
-#ifdef TTK_ENABLE_OPENMP
-#pragma omp parallel for num_threads(this->threadNumber_)
-#endif
-        for(size_t i = 0; i < nVertices; i++) {
-          // initialize average
-          outputData[i] = inputData[i];
-
-          // add neighbor values to average
-          size_t nNeighbors = triangulation->getVertexNeighborNumber(i);
-          ttk::SimplexId neighborId{-1};
-          for(size_t j = 0; j < nNeighbors; j++) {
-            triangulation->getVertexNeighbor(i, j, neighborId);
-            outputData[i] += inputData[neighborId];
-          }
-
-          // divide by neighbor number
-          outputData[i] /= (nNeighbors + 1);
-        }
-
-        // print the progress of the current subprocedure with elapsed time
-        this->printMsg("Computing Averages",
-                       1, // progress
-                       localTimer.getElapsedTime(), this->threadNumber_);
-      }
-
-      // ---------------------------------------------------------------------
-      // print global performance
-      // ---------------------------------------------------------------------
-      {
-        this->printMsg(ttk::debug::Separator::L2); // horizontal '-' separator
-        this->printMsg(
-          "Complete", 1, globalTimer.getElapsedTime() // global progress, time
-        );
-        this->printMsg(ttk::debug::Separator::L1); // horizontal '=' separator
-      }
-
-      return 1; // return success
-    }
-
-  }; // TopologicalMapper class
-
+    int execute(const std::vector<std::vector<double>> distMatrix, unsigned int nDim, std::vector<std::vector<double>> &outputCoords);
+  };
 } // namespace ttk

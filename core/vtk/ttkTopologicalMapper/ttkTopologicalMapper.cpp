5b34b24eed8ac6853b38a9c8dacb689c07012b06
Alexandre Talon <alexandre.talon@lip6.fr>
2023-05-24 22:40:51 +0200

    euh, la couche vtk topomap

---
+++ core/vtk/ttkTopologicalMapper/ttkTopologicalMapper.cpp
@@ -0,0 +1,208 @@
+#include <ttkTopologicalMapper.h>
+#include <vtkInformation.h>
+
+#include <vtkDataArray.h>
+#include <vtkDataSet.h>
+#include <vtkDataSetAttributes.h>
+#include <vtkTable.h>
+#include <vtkDoubleArray.h>
+#include <vtkInformation.h>
+#include <vtkIntArray.h>
+#include <vtkObjectFactory.h>
+#include <vtkPointData.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkNew.h>
+
+#include <ttkMacros.h>
+#include <ttkUtils.h>
+#include <ttkMapper.h>
+
+#include <regex>
+#include <string>
+
+vtkStandardNewMacro(ttkTopologicalMapper);
+
+
+static void extractInputMatrix(//ttk::Mapper::Matrix &inputMatrix,
+                               std::vector<std::vector<float>> &distMatrix,
+                               std::vector<std::string> &arrayNames,
+                               vtkDataSetAttributes *const pd,
+                               const std::string &regexp,
+                               const int nThreads,
+                               const bool useRegexp) {
+  if(pd == nullptr) {
+    return;
+  }
+
+  if(useRegexp) {
+    // select all input columns whose name is matching the regexp
+    arrayNames.clear();
+    const auto n = pd->GetNumberOfArrays();
+    arrayNames.reserve(n);
+    for(int i = 0; i < n; ++i) {
+      const auto name = pd->GetArrayName(i);
+
+      if(name == nullptr) {
+        continue;
+      }
+
+      // check regexp
+      if(!std::regex_match(name, std::regex(regexp))) {
+        continue;
+      }
+
+      // sanity check
+      const auto arr{pd->GetArray(name)};
+      if(arr == nullptr || arr->GetNumberOfComponents() != 1) {
+        continue;
+      }
+
+      // store matching array names
+      arrayNames.emplace_back(name);
+    }
+  }
+
+  std::vector<vtkDataArray *> arrays(arrayNames.size());
+  for(size_t i = 0; i < arrayNames.size(); ++i) {
+    arrays[i] = pd->GetArray(arrayNames[i].data());
+  }
+
+  //inputMatrix.alloc(pd->GetNumberOfTuples(), arrayNames.size());
+#ifdef TTK_ENABLE_OPENMP
+#pragma omp parallel for num_threads(nThreads)
+#endif // TTK_ENABLE_OPENMP
+  //for(size_t i = 0; i < inputMatrix.nRows(); ++i) {
+    //for(size_t j = 0; j < inputMatrix.nCols(); ++j) {
+  for(size_t i = 0; i < distMatrix.size(); ++i) {
+    for(size_t j = 0; j < distMatrix.size(); ++j) {
+      const auto array{arrays[j]};
+      distMatrix[i][j] = array->GetVariantValue(i).ToDouble();
+      //inputMatrix.get(i, j) = array->GetVariantValue(i).ToDouble();
+    }
+  }
+}
+
+ttkTopologicalMapper::ttkTopologicalMapper() {
+  this->SetNumberOfInputPorts(1);
+  this->SetNumberOfOutputPorts(1);
+}
+
+
+int ttkTopologicalMapper::FillInputPortInformation(int port, vtkInformation *info) {
+  if(port == 0) {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkTable"); // ou table
+    return 1;
+  }
+  return 0;
+}
+
+
+int ttkTopologicalMapper::FillOutputPortInformation(int port, vtkInformation *info) {
+  if(port == 0) {
+    info->Set( vtkDataObject::DATA_TYPE_NAME(), "vtkTable" );//table ?
+    return 1;
+  }
+  return 0;
+}
+/*
+static void setNodes(vtkUnstructuredGrid *const nodes,
+                     const std::vector<std::array<float, 3>> &compBaryCoords,
+                     const std::vector<int> &compBucketId) {
+
+  vtkNew<vtkIntArray> compId{};
+  compId->SetName("ComponentId");
+  compId->SetNumberOfComponents(1);
+  compId->SetNumberOfTuples(compBaryCoords.size());
+
+  vtkNew<vtkIntArray> bucket{};
+  bucket->SetName("BucketId");
+  bucket->SetNumberOfComponents(1);
+  bucket->SetNumberOfTuples(compBucketId.size());
+
+  vtkNew<vtkPoints> points{};
+  points->SetNumberOfPoints(compBaryCoords.size());
+  for(size_t i = 0; i < compBaryCoords.size(); ++i) {
+    points->SetPoint(i, compBaryCoords[i].data());
+    bucket->SetTuple1(i, compBucketId[i]);
+    compId->SetTuple1(i, i);
+  }
+
+  ttkUtils::CellVertexFromPoints(nodes, points);
+  nodes->GetPointData()->AddArray(bucket);
+  nodes->GetPointData()->SetScalars(compId);
+}
+*/
+
+
+
+
+
+int ttkTopologicalMapper::RequestData(vtkInformation *ttkNotUsed(request),
+                           vtkInformationVector **inputVector,
+                           vtkInformationVector *outputVector) {
+
+
+  auto input = vtkTable::GetData(inputVector[0]);
+  auto outputNodes = vtkTable::GetData(outputVector, 0);
+
+
+  /*
+  Matrix inputDistMat{};
+  if(this->ReEmbedMapper) {
+    ttk::Timer tm{};
+    extractInputMatrix(inputDistMat, this->DistanceMatrix,
+                       input->GetPointData(), this->DistanceMatrixRegexp,
+                       this->threadNumber_, this->SelectMatrixWithRegexp);
+    if(inputDistMat.nRows() != inputDistMat.nCols() || inputDistMat.nCols() == 0
+       || inputDistMat.nRows() == 0) {
+      this->printErr("Invalid input distance matrix");
+      return 0;
+    }
+    */
+  size_t nbPoint = input->GetNumberOfRows();
+  vtkNew<vtkPoints> outputPoints{};
+  outputPoints->SetNumberOfPoints(nbPoint);
+  outputPoints->GetData()->Fill(0.0);
+
+  std::vector<std::array<float, 3>> compBaryCoords{};
+
+  std::vector<std::vector<float>> distMatrix(nbPoint);
+  for (int i = 0; i < nbPoint; i++)
+    distMatrix[i].resize(nbPoint);
+  int status;
+
+  ttk::Mapper::Matrix inputDistMat{};
+  ttk::Timer tm{};
+  extractInputMatrix(//inputDistMat,
+                     distMatrix, this->DistanceMatrix,
+      input->GetRowData(), this->DistanceMatrixRegexp,
+      this->threadNumber_, this->SelectMatrixWithRegexp);
+  //if(inputDistMat.nRows() != inputDistMat.nCols() || inputDistMat.nCols() == 0
+  //    || inputDistMat.nRows() == 0) {
+  if (false) { //TODO
+    this->printErr("Invalid input distance matrix");
+    return 0;
+  }
+
+  this->execute(ttkUtils::GetPointer<float>(outputPoints->GetData()), distMatrix);
+  std::string nameCoords[3] = {"x", "y", "z"};
+  for (int i = 0; i < 3; i++)
+  {
+    vtkNew<vtkDoubleArray> col{};
+    col->SetNumberOfTuples(nbPoint);
+    col->SetName(nameCoords[i].c_str());
+    for (int j = 0; j < nbPoint; j++)
+      col->SetTuple1(j, ttkUtils::GetPointer<float>(outputPoints->GetData())[3*j+i]);
+
+    outputNodes->AddColumn(col);
+
+
+
+  }
+  //outputNodes->GetPointData()->SetNumberOfPoints(nbPoint);
+  //outputNodes->SetPoints(outputPoints);
+  //setNodes(outputNodes, compBaryCoords, compBucketId);
+  //setArcs(outputArcs, compArcs, outputNodes->GetPoints());
+
+  return 1;
+}
---
+++ core/vtk/ttkTopologicalMapper/ttkTopologicalMapper.h
@@ -0,0 +1,98 @@
+/// TODO 4: Provide your information and **update** the documentation (in
+/// particular regarding the order convention if input arrays need to be
+/// specified with the standard VTK call SetInputArrayToProcess()).
+///
+/// \ingroup vtk
+/// \class ttkTopologicalMapper
+/// \author Alexandre Talon <alexandre.talon@lip6.fr>
+/// \date June 2023
+///
+/// \brief TTK VTK-filter that wraps the ttk::TopologicalMapper module.
+///
+/// This class generates a mapper from a data-set. The mapper is computed using some minimum spanning tree
+/// and convex hull arguments.
+///
+/// \param Input vtkDataSet.
+/// \param Output vtkDataSet.
+///
+
+///
+/// The input data array needs to be specified via the standard VTK call
+/// vtkAlgorithm::SetInputArrayToProcess() with the following parameters:
+/// \param idx 0 (FIXED: the first array the algorithm requires)
+/// \param port 0 (FIXED: first port)
+/// \param connection 0 (FIXED: first connection)
+/// \param fieldAssociation 0 (FIXED: point data)
+/// \param arrayName (DYNAMIC: string identifier of the input array)
+///
+/// This filter can be used as any other VTK filter (for instance, by using the
+/// sequence of calls SetInputData(), Update(), GetOutput()).
+
+#pragma once
+
+// VTK Module
+#include <ttkTopologicalMapperModule.h>
+
+// ttk code includes
+#include <TopologicalMapper.h>
+#include <ttkAlgorithm.h>
+#include <ttkMacros.h>
+
+
+  //vtkTypeMacro(ttkTopologicalMapper, ttkAlgorithm);
+
+class TTKTOPOLOGICALMAPPER_EXPORT ttkTopologicalMapper
+  : public ttkAlgorithm ,  protected ttk::TopologicalMapper {
+private:
+  /**
+   * TODO 5: Add all filter parameters only as private member variables and
+   *         initialize them here.
+   */
+  std::string OutputArrayName{"AveragedScalarField"};
+
+public:
+
+  ttkSetEnumMacro(LowerDimension, LOWER_DIMENSION);
+  vtkGetEnumMacro(LowerDimension, LOWER_DIMENSION);
+
+
+  vtkSetMacro(SelectMatrixWithRegexp, bool);
+  vtkGetMacro(SelectMatrixWithRegexp, bool);
+
+  vtkSetMacro(DistanceMatrixRegexp, const std::string &);
+  vtkGetMacro(DistanceMatrixRegexp, std::string);
+
+  void SetDistMat(const std::string &s) {
+    this->DistanceMatrix.emplace_back(s);
+    this->Modified();
+  }
+  void ClearDistMat() {
+    this->DistanceMatrix.clear();
+    this->Modified();
+  }
+
+  static ttkTopologicalMapper *New();
+  vtkTypeMacro(ttkTopologicalMapper, ttkAlgorithm);
+
+protected:
+
+  ttkTopologicalMapper();
+  ~ttkTopologicalMapper() override = default;
+
+
+  int FillInputPortInformation(int port, vtkInformation *info) override;
+
+
+  int FillOutputPortInformation(int port, vtkInformation *info) override;
+
+
+  int RequestData(vtkInformation *request,
+                  vtkInformationVector **inputVector,
+                  vtkInformationVector *outputVector) override;
+
+private:
+  // domain vertices coordinates in high dimension
+  bool SelectMatrixWithRegexp{false};
+  std::string DistanceMatrixRegexp{".*"};
+  std::vector<std::string> DistanceMatrix{};
+};

cbdc7f0c5f23d8e5079f6ef66dc644fa96d64c40
Alexandre Talon <alexandre.talon@lip6.fr>
2023-08-17 16:05:51 +0200

    devrait passer les tests de présentation du code

--- core/vtk/ttkMapper/ttkMapper.h
+++ core/vtk/ttkMapper/ttkMapper.h
@@ -36,8 +36,8 @@
 #include <Mapper.h>
 #include <ttkAlgorithm.h>
 #include <ttkMacros.h>
-#include <vtkNew.h>
 #include <vtkIntArray.h>
+#include <vtkNew.h>
 #include <vtkUnstructuredGrid.h>
 
 class TTKMAPPER_EXPORT ttkMapper : public ttkAlgorithm, protected ttk::Mapper {
@@ -46,32 +46,28 @@ public:
 
   vtkTypeMacro(ttkMapper, ttkAlgorithm);
 
-  void SetNumberOfBuckets(int nbBuckets)
-  {
+  void SetNumberOfBuckets(int nbBuckets) {
     this->NumberOfBuckets = nbBuckets;
     this->firstTimeReembed_ = true;
     this->Modified();
   }
   vtkGetMacro(NumberOfBuckets, int);
 
-  void SetReEmbedMapper(bool enable)
-  {
+  void SetReEmbedMapper(bool enable) {
     this->ReEmbedMapper = enable;
     this->firstTimeReembed_ = true;
     this->Modified();
   }
   vtkGetMacro(ReEmbedMapper, bool);
 
-  void SetSelectMatrixWithRegexp (bool enable)
-  {
+  void SetSelectMatrixWithRegexp(bool enable) {
     this->SelectMatrixWithRegexp = enable;
     this->firstTimeReembed_ = true;
     this->Modified();
   }
   vtkGetMacro(SelectMatrixWithRegexp, bool);
 
-  void SetDistanceMatrixRegexp(const std::string &matName)
-  {
+  void SetDistanceMatrixRegexp(const std::string &matName) {
     this->DistanceMatrixRegexp = matName;
     this->firstTimeReembed_ = true;
     this->Modified();
@@ -87,7 +83,7 @@ public:
     this->Modified();
   }
 
-  void SetLowerDimension(unsigned int lowDim)//(LOWER_DIMENSION lowDim)
+  void SetLowerDimension(unsigned int lowDim) //(LOWER_DIMENSION lowDim)
   {
     this->LowerDimension = (LOWER_DIMENSION)lowDim;
     this->firstTimeReembed_ = true;
@@ -95,16 +91,16 @@ public:
   }
   vtkGetEnumMacro(LowerDimension, LOWER_DIMENSION);
 
-  void SetReductionAlgo(int redAlgo)//(REDUCTION_ALGO redAlgo)
+  void SetReductionAlgo(int redAlgo) //(REDUCTION_ALGO redAlgo)
   {
     this->ReductionAlgo = (REDUCTION_ALGO)redAlgo;
     this->firstTimeReembed_ = true;
     this->Modified();
   }
-  //ttkSetEnumMacro(ReductionAlgo, REDUCTION_ALGO);
+  // ttkSetEnumMacro(ReductionAlgo, REDUCTION_ALGO);
   vtkGetEnumMacro(ReductionAlgo, REDUCTION_ALGO);
 
-  void SetReembedMethod(int reembedMethod)//REEMBED_METHOD reembedMethod)
+  void SetReembedMethod(int reembedMethod) // REEMBED_METHOD reembedMethod)
   {
     this->ReembedMethod = (REEMBED_METHOD)reembedMethod;
     this->firstTimeReembed_ = true;
@@ -113,20 +109,19 @@ public:
   vtkGetEnumMacro(ReembedMethod, REEMBED_METHOD);
 
   vtkGetMacro(DilatationCoeff, double);
-  void SetDilatationCoeff(double coeff)
-  {
+  void SetDilatationCoeff(double coeff) {
     this->DilatationCoeff = coeff;
     ModifiedDilatation();
   }
-  
+
   void ModifiedDilatation() {
-    if (!firstTimeReembed_)
-    {
-      std::cerr <<"Modifying, yeah :-)";
+    if(!firstTimeReembed_) {
+      std::cerr << "Modifying, yeah :-)";
       needWholeUpdate_ = false;
-      ttkAlgorithm::Modified(); //Useless?
+      ttkAlgorithm::Modified(); // Useless?
     }
   }
+
 protected:
   ttkMapper();
 
@@ -145,8 +140,10 @@ private:
   bool SelectMatrixWithRegexp{false};
   std::string DistanceMatrixRegexp{".*"};
   std::vector<std::string> DistanceMatrix{};
-  bool needWholeUpdate_{true}; // To avoid to recompute everything when not necessary.
-  bool firstTimeReembed_{true}; // To avoid to recompute everything when not necessary. //Check sze f arrays...
+  bool needWholeUpdate_{
+    true}; // To avoid to recompute everything when not necessary.
+  bool firstTimeReembed_{true}; // To avoid to recompute everything when not
+                                // necessary. //Check sze f arrays...
   std::vector<float> pointsCoordsBackup_;
   vtkNew<vtkIntArray> connCompPrev_{}, bucketPrev_{};
   vtkNew<vtkUnstructuredGrid> arcsPrev_{}, nodesPrev_{};
